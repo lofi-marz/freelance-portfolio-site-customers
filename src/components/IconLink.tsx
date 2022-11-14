fa8152ea797010e52cc5072403e07c9d0399b1b0
Omari <marzipant.e@gmail.com>
2022-11-14 23:09:29 +0000

    Finally decided on a font

--- src/components/IconLink.tsx
+++ src/components/IconLink.tsx
@@ -9,7 +9,7 @@ export function IconLink({ href, children }: IconLinkProps) {
         <a
             key={href}
             href={href}
-            className="rounded from-primary to-secondary p-2 text-2xl transition-all hover:bg-gradient-to-r">
+            className="rounded p-2 text-2xl transition-all">
             {children}
         </a>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import Head from 'next/head';
-import { Inter, Poppins, Rubik, Merriweather } from '@next/font/google';
+import {
+    Inter,
+    Poppins,
+    Rubik,
+    Merriweather,
+    IBM_Plex_Sans,
+} from '@next/font/google';
 import Image from 'next/image';
 import me from './me.jpg';
 import clsx from 'clsx';
@@ -14,8 +20,13 @@ import {
 import { IconLink } from '@/components/IconLink';
 import { motion, Variants } from 'framer-motion';
 //const title = Poppins({ weight: ['600', '700', '800', '900'] });
-const title = Rubik();
-const text = Inter();
+const title = IBM_Plex_Sans({
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
+});
+const text = Inter({
+    subsets: ['latin'],
+});
 
 const headingVariants: Variants = {
     hidden: { opacity: 0, y: -10 },
@@ -27,35 +38,23 @@ const underlineVariants: Variants = {
     visible: { width: '100%' },
 };
 
-function MyContent() {
-    return (
-        <div>
-            <h1 className={title.className}>New Features in Next.js 13</h1>
-            <p className={text.className}>
-                A new version of Next.js has been released, featuring some great
-                improvements for performance and usability, as well as some
-                interesting new features, which Ill talk you through in this
-                article.
-            </p>
-        </div>
-    );
-}
-
 function Title() {
     return (
         <motion.div
             initial="hidden"
             animate="visible"
-            transition={{ staggerChildren: 0.1 }}>
+            transition={{ staggerChildren: 0.2 }}>
             <h1
                 className={clsx(
-                    'text-5xl font-extrabold md:text-center',
+                    'flex flex-col gap-2 text-8xl font-bold',
                     title.className
                 )}>
-                <motion.div variants={headingVariants}>Omari</motion.div>
-                <motion.div variants={headingVariants}>Thompson</motion.div>
+                <motion.div variants={headingVariants}>Hi,</motion.div>
+                <motion.div variants={headingVariants}>
+                    I&apos;m Omari.
+                </motion.div>
             </h1>
-            <div className="h-2 w-3/5 md:mx-auto">
+            <div className="h-3 w-3/5">
                 <motion.div
                     className="h-full w-full bg-red-400 md:mx-auto"
                     variants={underlineVariants}></motion.div>
@@ -66,7 +65,7 @@ function Title() {
 
 function Socials() {
     return (
-        <div className="flex w-full items-center justify-center gap-2">
+        <div className="flex w-full items-center justify-center gap-2 rounded md:flex-col md:bg-red-400 md:text-white">
             <IconLink href="mailto:othompsonedwards@gmail.com">
                 <FaBook />
             </IconLink>
@@ -85,14 +84,17 @@ function Socials() {
 
 function VideoBackground() {
     return (
-        <div className="h-full w-full">
+        <div className="relative h-full w-full">
             <video
-                className="w-full object-cover brightness-[.8] saturate-50 md:h-full"
+                className="w-full object-cover brightness-[.8] saturate-[.8] md:h-full"
                 autoPlay
                 loop
                 muted>
                 <source src="/loop.mp4" type="video/mp4" />
             </video>
+            <div className="absolute right-0 top-0 z-10 mr-4 flex h-full items-center justify-center md:hidden">
+                <Socials />
+            </div>
         </div>
     );
 }
@@ -109,29 +111,39 @@ export default function Home() {
             </div>
             <main
                 className={clsx(
-                    'flex h-full max-w-2xl flex-col items-start justify-start gap-10 p-10 text-[#505462] md:w-1/2 md:items-center md:justify-center',
+                    'flex h-full max-w-2xl flex-col items-center justify-center gap-10 p-10 text-stone-700 dark:bg-stone-900 dark:text-white md:w-1/2',
                     text.className
                 )}>
-                <div className="mx-auto aspect-[2/1] w-1/2 overflow-visible">
-                    <Image
-                        className="mx-auto w-full rounded drop-shadow-lg"
-                        src={me}
-                        alt="me"
-                    />
-                </div>
-                <Title />
-                <p className="mx-auto text-center opacity-90 lg:w-2/3">
-                    Nottingham-based freelance web design and development. I
-                    specialise in bespoke sites, from small-scale landing pages,
-                    to larger web apps.
-                </p>
-                <button className="mx-auto rounded bg-red-400 px-10 py-3 text-center text-white">
-                    Get in touch
-                </button>
-                <div className="mx-auto w-2/3 ">
-                    <Socials />
+                <div className="flex h-full w-full flex-col items-start justify-center gap-10">
+                    <Title />
+                    <p className="w-full text-center opacity-90 dark:opacity-100 md:text-start">
+                        Nottingham-based freelance web design and development. I
+                        specialise in bespoke sites, from small-scale landing
+                        pages, to larger web apps.
+                    </p>
+                    <button
+                        className={clsx(
+                            'mx-auto rounded bg-red-400 px-10 py-3 text-center font-bold text-white md:mx-0',
+                            title.className
+                        )}>
+                        Get in touch.
+                    </button>
                 </div>
             </main>
+        </div>
+    );
+}
+
+function LoadingScreen() {}
+
+function Me() {
+    return (
+        <div className="mx-auto aspect-[2/1] w-1/2 overflow-visible">
+            <Image
+                className="mx-auto w-full rounded drop-shadow-lg"
+                src={me}
+                alt="me"
+            />
         </div>
     );
 }
