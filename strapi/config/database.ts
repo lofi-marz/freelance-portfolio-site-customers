ac2e71cfc503349907a499c9e79f1bc3edb91864
Omari <marzipant.e@gmail.com>
2022-12-09 09:41:06 +0000

    More server changes

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,11 +1,16 @@
-import path from 'path';
-
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
+    client: 'postgres', 
+  connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+        ssl: {
+          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+       },
+      },
+      debug: false,
   },
 });
--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -4,4 +4,5 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('URL', 'https://marimari.tech/cms')
 });
