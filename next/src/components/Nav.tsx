6b818ffa89c802fd3d6854a62192e84059fed6cc
Omari <marzipant.e@gmail.com>
2023-01-25 00:31:29 +0000

    Improvements to project page

--- next/src/components/Nav.tsx
+++ next/src/components/Nav.tsx
@@ -8,7 +8,7 @@ export function Nav() {
     return (
         <nav
             className={clsx(
-                'fixed top-0 z-10 flex h-16 w-full items-center justify-center uppercase',
+                'fixed top-0 z-50 flex h-16 w-full items-center justify-center uppercase',
                 title.className
             )}>
             <ul className="flex w-full items-center justify-between px-16 text-sm font-bold md:justify-center md:gap-16">
--- next/src/components/sections/projects/Projects.tsx
+++ next/src/components/sections/projects/Projects.tsx
@@ -6,11 +6,13 @@ import { text, title } from '../../../fonts';
 import { FaGithub, FaLink } from 'react-icons/fa';
 import { useStrapiContentContext } from '@/components/StrapiContextProvider';
 import { ProjectContent } from 'utils/strapi';
+import { motion, Variants } from 'framer-motion';
+import { WithChildrenProps } from 'types';
 
 type ProjectProps = ProjectContent['attributes'] & { odd?: boolean };
 
 export function Projects() {
-    const {projects} = useStrapiContentContext()!;
+    const { projects } = useStrapiContentContext()!;
     console.log('Projects:', projects);
     return (
         <section
@@ -20,13 +22,37 @@ export function Projects() {
             )}
             id="projects">
             <h2 className="text-7xl font-bold">
-                projects <div className="-mt-1 h-4 w-2/3 bg-primary"></div>
+                stuff I&apos;ve made<div className="-mt-1 h-4 w-2/3 bg-primary"></div>
             </h2>
             <ul className="flex w-full flex-col gap-32 p-16">
-                {projects.map((p) => <Project key={p.id} {...p.attributes} />)}
+                {projects.map((p, i) => <Project key={p.id} {...p.attributes} odd={i % 2 == 1}/>)}
             </ul>
         </section>
     );
+}
+
+const underlineVariants: Variants = {
+    hidden: {width: '0%'},
+    show: {width: '100%'}
+}
+
+
+const verticalUnderlineVariants: Variants = {
+    hidden: {height: '0%'},
+    show: {height: '100%', transition: {ease: 'easeOut'}}
+}
+
+function ProjectHeader({children, odd}: {odd: boolean} & WithChildrenProps) {
+    return (<div className={clsx('text-5xl font-bold w-18 lowercase flex items-start content-end justify-center flex-col', !odd && 'rotate-180')}    style={{ writingMode: 'vertical-rl' }}>
+    <h3 className="z-10">{children}</h3>
+    <div className="-mr-3 w-4 h-2/3 bg-primary -z-5">
+        <motion.div className="w-full h-full bg-primary" variants={verticalUnderlineVariants} initial="hidden" whileInView="show" exit="hidden"/>
+    </div>
+</div>)
+}
+
+function ProjectBrief({children}: WithChildrenProps) {
+    
 }
 
 function Project({
@@ -39,21 +65,18 @@ function Project({
     mobilePreview,
     odd = false,
 }: ProjectProps) {
-    console.log('Desktop:', desktopPreview);
+    console.log(description);
     return (
         <li
             className={clsx(
-                'flex items-start justify-center gap-16 h-96',
+                'flex items-start justify-between gap-16 h-96',
                 odd && 'flex-row-reverse'
             )}>
-            <div className="mt-16 flex w-2/3 flex-col gap-8">
-                <div className="text-5xl font-bold">
-                    {title}
-                    <div className="-mt-3 h-4 w-1/2 bg-primary"></div>
-                </div>
-                <div className={clsx('text-xl', text.className)}>
-                    {description}
-                </div>
+            <ProjectHeader odd={odd}>{title}</ProjectHeader>
+            <div className={clsx('mt-16 flex w-[36rem]  flex-col gap-8 h-full  z-10', odd ? '-ml-32' : '-mr-32')}>
+                <p className={clsx('p-4 bg-primary text-xl whitespace-pre-line', text.className)}>
+                    {brief}
+                </p>
                 <div className="flex flex-row text-xl font-bold">
                     <a
                         className="themed-bg-invert themed-text-invert p-2 px-4"
@@ -69,8 +92,8 @@ function Project({
                     )}
                 </div>
             </div>
-            <div className="w-full h-full relative">
-                <Image src={'https://marimari.tech/cms'+desktopPreview.data.attributes.url} alt="" fill className="object-contain"></Image>
+            <div className="h-full relative flex items-center justify-center rounded overflow-visible w-[48rem]">
+                <Image src={'https://marimari.tech/cms' + desktopPreview.data.attributes.url} alt="" fill className="object-contain" />
             </div>
         </li>
     );
