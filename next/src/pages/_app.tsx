72d0d2009f02080713f4987287c8771a6e21bb7d
Omari <marzipant.e@gmail.com>
2023-03-10 14:46:39 +0000

    Trying Vercel analytics

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -2,15 +2,16 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { DarkModeContextProvider } from '@/components/DarkModeContextProvider';
 import Head from 'next/head';
-
+import { Analytics } from '@vercel/analytics/react';
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <DarkModeContextProvider>
-                      <Head>
-                        <title>Omari</title>
-                        <link rel="icon" href="/favicon.ico" />
-                    </Head>
+            <Head>
+                <title>Omari</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <Component {...pageProps} />
+            <Analytics />
         </DarkModeContextProvider>
     );
 }
