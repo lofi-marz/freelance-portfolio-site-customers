183ddabcea82ab971bda8e2be56c475af112eaf5
Omari <marzipant.e@gmail.com>
2023-05-04 20:09:56 +0000

    Update strapi

---
+++ strapi/src/api/search/controllers/search.ts
@@ -0,0 +1,7 @@
+/**
+ * search controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::search.search');
---
+++ strapi/src/api/search/routes/search.ts
@@ -0,0 +1,7 @@
+/**
+ * search router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::search.search');
